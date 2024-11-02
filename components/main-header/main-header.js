fcd2b3e4e3736855268e850633432fa5f9647466
mohamuhsin <muhsinohoho@outlook.com>
2024-11-03 01:55:18 +0300

    added custom main header background component

---
+++ components/main-header/main-header.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImage from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header.backgorund";
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image
+                        width="1024"
+                        height="1024"
+                        src={logoImage.src}
+                        alt="the foodie logo"
+                        priority
+                    />
+                    NextLevel Food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Foodies Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
