2ce7668f85a19bae5dfe8503585edab9597abb47
mohamuhsin <muhsinohoho@outlook.com>
2024-11-15 21:53:03 +0300

    changed reactDom.useFormState to React.useActionState

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useFormState } from "react-dom";
+import { useActionState } from "react";
 
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
@@ -8,7 +8,7 @@ import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
-    const [state, formAction] = useFormState(shareMeal, { message: null });
+    const [state, formAction] = useActionState(shareMeal, { message: null });
     return (
         <>
             <header className={classes.header}>
